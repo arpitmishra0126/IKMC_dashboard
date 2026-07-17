ce0ccc323cf2dcc9c5fb6b400aaa83195a12c41e
Arpit Mishra <arpit.mishra@celworld.org>
2026-07-17 16:47:24 +0530

    dark light theme font setter

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+from pathlib import Path
+def load_css():
+    css_file = Path("assets/styles.css")
+
+    if css_file.exists():
+        with open(css_file) as f:
+            st.markdown(
+                f"<style>{f.read()}</style>",
+                unsafe_allow_html=True,
+            )
+            
 import streamlit as st
 
 from components.kpi_cards import kpi_card
@@ -11,6 +22,8 @@ st.set_page_config(
     page_title="IKMC Dashboard",
     layout="wide"
 )
+
+
 
 # ==================================================
 # HEADER
